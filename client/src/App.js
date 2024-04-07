8df86b4fd7e3f3f9f11ac959866d54611359ccce
Sayaka Chishiki Jakobsson <sc222rb@student.lnu.se>
2024-04-07 23:20:19 +0200

    fix basic client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout.js';
+import LogIn from './components/Login.js';
+import Signup from './components/Signup.js';
+import ProgressTracking from './components/ProgressTracking.js';
+import Home from './components/Home.js';
+import About from './components/About.js';
+import NoMatch from './components/NoMatch.js';
 
 const App = () => {
-  return <div>test</div>
+
+  return (
+    <Router>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="login" element={<LogIn />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="progressTracking" element={<ProgressTracking />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the Hello Nihongo Kids!</p>
+    </div>
+  )
+}
+
+export default Home

a5d7d146abe2b7e0fb05607fbdb150e4e46bef57
Sayaka Chishiki Jakobsson <sc222rb@student.lnu.se>
2024-04-10 09:04:30 +0200

    Edite ProgressTracking interface

--- client/src/components/ProgressTracking.js
+++ client/src/components/ProgressTracking.js
@@ -4,7 +4,10 @@ const ProgressTracking = ({ progress }) => {
   return (
     <div>
       <h1>Progress Tracking</h1>
-      <p>Progress: {progress}%</p>
+      <p>Hiragana to Latin: 8/60</p>
+      <p>Katakana to Latin: 7/60</p>
+      <p>Hiragana to Katakana: 6/60</p>
+      <p>Katakana to Hiragana: 5/60</p>
     </div>
   )
 }

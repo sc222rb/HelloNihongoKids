aed3973a16e7d24cba6018bd74fcf3cdd238b140
Sayaka Chishiki Jakobsson <sc222rb@student.lnu.se>
2024-05-04 12:54:53 +0200

    Creating card and card grit

--- client/src/components/Games/Games.js
+++ client/src/components/Games/Games.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import './Games.css';
+import SingleCard from '../SingleCard/SingleCard.js'
 
+const cardImages = [
+  { "src": "./img/hiragana/a-gyou/a.png" },
+  { "src": "./img/hiragana/a-gyou/i.png" },
+  { "src": "./img/hiragana/a-gyou/u.png" },
+  { "src": "./img/hiragana/a-gyou/e.png" },
+  { "src": "./img/hiragana/a-gyou/o.png" },
+  { "src": "./img/katakana/a-gyou/a.png" },
+  { "src": "./img/katakana/a-gyou/i.png" },
+  { "src": "./img/katakana/a-gyou/u.png" },
+  { "src": "./img/katakana/a-gyou/e.png" },
+  { "src": "./img/katakana/a-gyou/o.png" },
+]
 const Games = () => {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  // shuffle cards for new game
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <Container className="py-5">
       <Row className="justify-content-center">
         <Col xs={12} md={8}>
-          <h1 className="text-center mb-4">Games</h1>
-          <p className="text-center">Hiragana to Latin Alphabet Match</p>
-          <p className="text-center">Katakana to Latin Alphabet Match</p>
-          <p className="text-center">Hiragana to Katakana Match</p>
-          <p className="text-center">Katakana to Hiragana Match</p>
+          <div className="Games">
+            <h1>Hiragana and Katakna Match</h1>
+            <button onClick={shuffleCards}>New Game</button>
+
+            <div className="card-grid">
+              {cards.map(card => (
+                <SingleCard
+                  key={card.id}
+                  card={card}
+                />
+              ))}
+            </div>
+          </div>
         </Col>
       </Row>
     </Container>

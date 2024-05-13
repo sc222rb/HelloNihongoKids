a7c064bd20392d90681cae3a1359d565beee87ad
Sayaka Chishiki Jakobsson <sc222rb@student.lnu.se>
2024-05-14 01:58:10 +0200

    add GameController

--- server/src/controllers/api/game-controller.js
+++ server/src/controllers/api/game-controller.js
@@ -5,9 +5,40 @@
  * @version 1.0.0
  */
 
-/**
-* Encapsulates a controller.
-*/
-export class GameController {
+import { GameStatsModel } from '../../models/GameStatsModel.js'
 
+/**
+ * Encapsulates a controller.
+ */
+export class GameController {
+  /**
+   * Save game statistics to the database.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   * @returns {Promise<void>} A Promise representing the completion of the operation.
+   */
+  async saveStats (req, res, next) {
+    try {
+      console.log(req.body.selectedColumnName)
+      const gameStats = new GameStatsModel({
+        column: req.body.selectedColumnName,
+        turns: req.body.turns
+      })
+
+      await gameStats.save()
+
+      const location = new URL(
+        `${req.protocol}://${req.get('host')}${req.baseUrl}/${gameStats._id}`
+      )
+
+      res
+        .location(location.href)
+        .status(201)
+        .json(gameStats)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
